0e8e47b96fba710b337d8af608586be07039e0a5
Nick Messing <dot.nick.dot.messing@gmail.com>
2017-03-10 12:08:35 +1300

    much changes, such wow

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
 # fs-multi-copy
 Node.JS copy multiple files
 
-  	var fsMultiCopy = require('fs-multi-copy')
-  	fsMultiCopy([
-  	  {
-  	    from: 'tmp.0',
-  	    to: 'tmp.0.out'
-  	  },
-  	  {
-  	    from: 'tmp.1',
-  	    to: 'tmp.1.out'
-  	  }
-  	], function (err) {
-  	  if (err) {
-  	  	throw err
-  	  }
-  	  //files copied
-  	})
+### Installation
+
+```
+npm install fs-multi-copy -S
+```
+or
+```
+yarn add fs-multi-copy
+```
+
+### How to use
+
+```js
+const mc = require('fs-multi-copy')
+
+mc([
+  {
+    from: 'tmp.0',
+    to: 'tmp.0.out'
+  },
+  {
+    from: 'tmp.1',
+    to: 'tmp.1.out'
+  }
+]).then(() => {
+  // copied
+}).catch(err => {
+  // something wrong happened
+})
--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
-var fsMultiCopy = require('../lib/index.js')
+const fs = require('fs')
+const mc = require('../lib/index.js')
 
 fs.writeFileSync('tmp.0', '0')
 fs.writeFileSync('tmp.1', '1')
 
-fsMultiCopy([
+mc([
   {
     from: 'tmp.0',
     to: 'tmp.0.out'
@@ -13,11 +13,7 @@ fsMultiCopy([
     from: 'tmp.1',
     to: 'tmp.1.out'
   }
-], function (err) {
-  if (err) {
-    throw err
-  }
-
+]).then(() => {
   if (fs.readFileSync('tmp.0.out', 'utf8') === '0' && fs.readFileSync('tmp.1.out', 'utf8') === '1') {
     console.log('Success')
   } else {
@@ -28,4 +24,6 @@ fsMultiCopy([
   fs.unlinkSync('tmp.1')
   fs.unlinkSync('tmp.0.out')
   fs.unlinkSync('tmp.1.out')
+}).catch(err => {
+  throw err
 })
